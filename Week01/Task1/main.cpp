c33d12ae268deb5bae0cf9a6a16aa1beb1cbc98a
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2019-02-11 17:07:31 +0200

    Added task2

--- Week01/Task1/main.cpp
+++ Week01/Task1/main.cpp
@@ -5,17 +5,31 @@
 
 using namespace std;
 
+const int MAXIMUM_BITS = 63;
+const int EXPONENT_BITS = 7;
+const int MANTISSA_BITS = 40;
+const int FLOAT_BITS = 48;
+
 
 class LargeFloat {
 
 private:
 	bitset<1> sign;
-	bitset<8> exponent;
-	bitset<40> mantissa;
+	bitset<EXPONENT_BITS> exponent;
+	bitset<MANTISSA_BITS> mantissa;
 
 	void setMantissa(double value)
 	{
+		for (int i = 0; i < MANTISSA_BITS; i++)
+		{
+			value *= 2;
 
+			if (value > 1)
+			{
+				value -= 1;
+				mantissa |= (1ll << i);
+			}
+		}
 	}
 
 	void setSign(double value)
@@ -37,34 +51,45 @@ private:
 			value /= 2;
 			power++;
 		}
-		this->exponent = 127 + power;
+		this->exponent = MAXIMUM_BITS + power;
 		setMantissa(value - 1);
 	}
 
 public:
 
-	friend ostream & operator << (ostream &stream, const LargeFloat &value) {
-		stream << 5;
+	bitset<MANTISSA_BITS> getMantissa() {
+
+		return mantissa;
 	}
 
-	friend LargeFloat operator+(const LargeFloat & a, const LargeFloat & b) {
-
+	friend ostream & operator << (ostream &stream, const LargeFloat &value)
+	{
+		bitset<FLOAT_BITS> floatNumber;
+		floatNumber |= bitset<FLOAT_BITS>(value.mantissa.to_ullong());
+		floatNumber |= (bitset<FLOAT_BITS>(value.exponent.to_ullong()) << 39);
+		floatNumber |= (bitset<FLOAT_BITS>(value.sign.to_ullong()) << 47);
+		
+		return stream;
 	}
 
-
+	friend LargeFloat operator+(const LargeFloat & a, const LargeFloat & b)
+	{
+	}
 
 	LargeFloat(double value) {
 		setSign(value);
-		this->sign = 0;
-		this->exponent = 0;
-		this->mantissa = 0;
 	}
 
 };
 
 int main() {
 
-	LargeFloat flot = LargeFloat(173.7);
+	LargeFloat flot = LargeFloat(173.7) + LargeFloat(10);
+
+
+	cout << flot;
+
+	system("pause");
 
 	return 0;
 }
